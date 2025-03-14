dfb37b87a708dfb66442142f886cd5e1b08652e7
haileybronson <haileybronsonphotos@gmail.com>
2025-03-13 18:11:43 -0600

    finished !

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,33 +1,34 @@
-import axios from 'axios';
+import axios from "axios"
 import API_URL from "./env"
-import authHeader from './auth-header';
+import authHeader from "./auth-header"
 class UserService {
-
-    getUser() {
-        return axios.get(API_URL + 'user',
-            { headers: authHeader() })
-            .then(response => {
-                console.log(response)
-                return response.data.results;
-            });
-    }
-    uploadAvatar(image) {
-        let formData = new FormData();
-        formData.append('image', image);
-        return axios.post(API_URL + 'user/upload_avatar', formData,
-            { headers: authHeader('multipart') })
-            .then(response => {
-                return response.data.results;
-            });
-    }
-    removeAvatar() {
-        return axios.delete(API_URL + 'user/remove_avatar',
-            { headers: authHeader() })
-            .then(response => {
-                return response.data.results;
-            });
-    }
+	getUser() {
+		return axios
+			.get(API_URL + "user", { headers: authHeader() })
+			.then((response) => {
+				console.log(response)
+				return response.data.data
+			})
+	}
+	uploadAvatar(image) {
+		let formData = new FormData()
+		formData.append("image", image)
+		return axios
+			.post(API_URL + "user/upload_avatar", formData, {
+				headers: authHeader("multipart")
+			})
+			.then((response) => {
+				return response.data.data
+			})
+	}
+	removeAvatar() {
+		return axios
+			.delete(API_URL + "user/remove_avatar", { headers: authHeader() })
+			.then((response) => {
+				return response.data.data
+			})
+	}
 }
 
-const userService = new UserService();
-export default userService;
+const userService = new UserService()
+export default userService
